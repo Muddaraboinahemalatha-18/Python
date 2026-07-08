4ac933bf8c75d00e4b0253da50143d0ffdfee794
Muddaraboinahemalatha-18 <hemalathamuddaraboina@gmail.com>
2026-07-08 15:47:18 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,54 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Datatypes
+>>> a=5
+>>> type(a)
+<class 'int'>
+>>> b=7.2
+>>> type(b)
+<class 'float'>
+>>> c='name'
+>>> type(c)
+<class 'str'>
+>>> d="code"
+>>> tpe(d)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    tpe(d)
+NameError: name 'tpe' is not defined. Did you mean: 'type'?
+>>> d="cpde"
+>>> type(d)
+<class 'str'>
+>>> e='''codegnan'''
+>>> type(e)
+<class 'str'>
+>>> f=3+5j
+>>> type(f)
+<class 'complex'>
+g=8j+2
+type(g)
+<class 'complex'>
+h=12j
+type(h)
+<class 'complex'>
+a=j
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    a=j
+NameError: name 'j' is not defined
+i=True
+type(i)
+<class 'bool'>
+j=False
+type(j)
+<class 'bool'>
+k=true
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    k=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+l=false
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    l=false
+NameError: name 'false' is not defined. Did you mean: 'False'?
