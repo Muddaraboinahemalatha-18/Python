f84c3d24753cd6c0f4fddd0d1c1a98bf33480b0d
Muddaraboina Hemalatha <hemalathamuddaraboina@gmail.com>
2026-07-10 16:04:46 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,49 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Indexing
+>>> #Positive
+>>> a= "Vijayawada"
+>>> a[0]
+'V'
+>>> a[5]
+'a'
+>>> a[6]
+'w'
+>>> a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'Vijaya'
+>>> b="I am in class"
+>>> b[1]
+' '
+>>> b[2]+b[3]
+'am'
+>>> b[1]+b[4]+b[7]
+'   '
+>>> c="I am learning python"
+>>> c[14]+c[15]+c[16]+c[17]+c[18]+c[19]
+'python'
+>>> c[5]+c[6]+c[7]+c[8]+c[9]
+'learn'
+>>> d="I love codegnan"
+d[7]+d[8]+d[9]+d[10]
+'code'
+d[2]+d[3]+d[4]+d[5]
+'love'
+d[11]+d[12]+d[13]+d[14]
+'gnan'
+#Negative
+v="Vijayawada is a royal city"
+v[-6]+v[-7]+v[-8]+v[-9]+v[-10]
+'layor'
+v[-10]+v[-9]+v[-8]+v[-7]+v[-6]
+'royal'
+v[-4]+v[-3]+v[-2]+v[-1]
+'city'
+v[-15]+v[-14]
+'is'
+h="Vizag is a city of destiny"
+h[-7]+h[-6]+h[-6]+h[-5]+h[-4]+h[-3]+h[-2]+h[-1]
+'deestiny'
+h[-26]+h[-25]+h[-24]+h[-23]+h[-22]
+'Vizag'
+h[-7]+h[-6]+h[-5]+h[-4]+h[-3]+h[-2]+h[-1]
+'destiny'
